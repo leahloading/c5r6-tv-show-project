44d11db08ca01b2d6b945a9a1d84630c63e329a5
leah <theloadingpage@gmail.com>
2022-08-01 18:30:10 +0100

    update EpisodeCard to use utilities with tests

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -1,11 +1,32 @@
 import Episode from "../types/Episode";
+import generateEpisodeCode from "../utils/generateEpisodeCode";
+import removeEpNameSpace from "../utils/removeEpNameSpace";
+import removePTags from "../utils/removePTags";
 
 interface Prop {
   episode: Episode;
 }
 
 function EpisodeCard({ episode }: Prop): JSX.Element {
-  return <p key={episode.id}>{JSON.stringify(episode)}</p>;
+  return (
+    <article
+      id={`${removeEpNameSpace(episode)}-${generateEpisodeCode(
+        episode
+      )}`.toLowerCase()}
+    >
+      <h3>
+        {episode.name} <span>{generateEpisodeCode(episode)}</span>
+      </h3>
+      <p>{removePTags(episode)}</p>
+      <img src={episode.image.medium} alt={`${episode.name}`} />
+    </article>
+  );
 }
 
 export default EpisodeCard;
+
+// [x] the episode's name
+// [x] the season number (see below)
+// [x] the episode number (see below)
+// [x] the episode's medium-sized image
+// [x] the episode's summary text (see below)
