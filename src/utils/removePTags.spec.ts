428483d7df66def7004a2b33fe73e56734bffa96
Bhawick Jain <bhawick@gmail.com>
2022-08-05 20:25:37 +0100

    adjust removePTag test input type

--- src/utils/removePTags.spec.ts
+++ src/utils/removePTags.spec.ts
@@ -7,7 +7,3 @@ test("removePTags returns the episode summary without HTML P tags", () => {
     "Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage."
   );
 });
-
-// test("getData returns the expected length", () => {
-//     expect(getData()).toHaveLength(73);
-//   });
